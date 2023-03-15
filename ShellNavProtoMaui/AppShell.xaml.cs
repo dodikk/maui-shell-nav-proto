57bdd20e07ef682959c0d62e1d65e88c7011e9fa
Oleksandr Dodatko <dodikk88.reg@gmail.com>
2023-03-15 22:50:28 +0200

    [shell] register pages

--- ShellNavProtoMaui/AppShell.xaml.cs
+++ ShellNavProtoMaui/AppShell.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using ShellNavProtoMaui.Views.Login;
-
+using ShellNavProtoMaui.Views.Tabs;
 
 namespace ShellNavProtoMaui;
 
@@ -34,18 +34,45 @@ public partial class AppShell : Shell
         // TODO: [alex-d] maybe replace with |//login/PinCodePage|
         // -
         await Shell.Current.GoToAsync("//login/WelcomePage");
+        // await Shell.Current.GoToAsync("//Main");
 
-        //await Shell.Current.GoToAsync("//login/PinCodePage");
+
+        // ???
+        // await Shell.Current.GoToAsync("//login/PinCodePage");
         // await Shell.Current.GoToAsync("//Main/TabDashboard");
+
     }
 
     private static void InitializeRouting()
     {
+        // note: [alex-d] root pages registered in xaml
+        // ---
+        // * WelcomePage - "//login/WelcomePage"
+        // * PinCodePage - "//login/PinCodePage"
+        // 
+        // * Main        - <TabBar Route="Main">
+        // * Main/TabDashboard
+        // * Main/TabWallets
+        // * Main/TabOthers
+        // -
+
+
+        // TODO: [alex-d] maybe just "LoginPage" instead of path with separators
+        // -
         Routing.RegisterRoute(
               route: "login/WelcomePage/LoginPage"
             , type: typeof(PinCodePage)
         );
-            
+
+        Routing.RegisterRoute(
+              route: nameof(ActivityPage)
+            , type: typeof(PinCodePage)
+        );
+
+        Routing.RegisterRoute(
+              route: nameof(SettingsPage)
+            , type: typeof(SettingsPage)
+        );
     }
 }
 
