374409acc06a90b7baa8a8b4fc924f9f43c2088e
Oleksandr Dodatko <dodikk88.reg@gmail.com>
2023-03-16 14:52:22 +0200

    [dashboard] coin action buttons panel

---
+++ ShellNavProtoMaui/Views/Controls/CoinActionsBar.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+
+namespace ShellNavProtoMaui.Views.Controls;
+
+
+public partial class CoinActionsBar : ContentView
+{
+	public CoinActionsBar()
+	{
+		InitializeComponent();
+	}
+
+	[RelayCommand]
+	private void OnReceiveButtonTapped()
+	{
+        _ = Shell.Current.GoToAsync("ReceiveCoinPage");
+	}
+
+    [RelayCommand]
+    private void OnSendButtonTapped()
+    {
+        _ = Shell.Current.GoToAsync("SendCoinPage");
+    }
+
+    [RelayCommand]
+    private void OnConvertButtonTapped()
+    {
+        _ = Shell.Current.GoToAsync("ConvertCoinPage");
+    }
+}
